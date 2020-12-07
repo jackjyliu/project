b88590b31044e0fce6d36be17dc986614ef91a16
Jack <jieyuliu@live.com>
2020-12-06 22:37:11 -0500

    renamed files to be more organized

--- sql_load.py
+++ sql_load.py
@@ -1,21 +1,21 @@
 """
 functions to load data into postgresql database
 """
-import toronto_data
+import data_toronto
 import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_batch
-import win_psql as w
+import sql_win_login as psql
 import datetime
 
 def sql_write(user, password, host, port, database, query, records):
     
     # connect to database
     try: 
-        connection = psycopg2.connect(user=w.user,
-                                        password=w.password,
-                                        host=w.host,
-                                        port=w.port,
+        connection = psycopg2.connect(user=psql.user,
+                                        password=psql.password,
+                                        host=psql.host,
+                                        port=psql.port,
                                         database=database)
         cursor = connection.cursor()
         
@@ -41,10 +41,10 @@ def insert_toronto_power():
     """
     insert toronto power data for rows not in already in database 
     """
-    connection = psycopg2.connect(user=w.user,
-                                    password=w.password,
-                                    host=w.host,
-                                    port=w.port,
+    connection = psycopg2.connect(user=psql.user,
+                                    password=psql.password,
+                                    host=psql.host,
+                                    port=psql.port,
                                     database='toronto')
     cursor = connection.cursor()
 
@@ -56,14 +56,14 @@ def insert_toronto_power():
     last_ts = last_record['ts'][0]
 
     # filter records to only new data
-    power_data = toronto_data.toronto_power(start_year=max(2004,last_ts.year))
+    power_data = data_toronto.toronto_power(start_year=max(2004,last_ts.year))
     new_power_data = power_data[power_data['ts'] > last_ts]
 
     write_db = 'toronto'
     query = 'INSERT INTO power (ts, power_use_mwh) VALUES (%s, %s);'
     records = [tuple(x) for x in new_power_data.to_numpy()]
 
-    sql_write(w.user, w.password, w.host, w.port, write_db, query, records)
+    sql_write(psql.user, psql.password, psql.host, psql.port, write_db, query, records)
     
     print('toronto power load complete')
     return True
