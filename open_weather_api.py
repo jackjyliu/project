534d1f436c75818efb0caedd1aa0552c99fb1fea
Jack <jack.jy.liu@gmail.com>
2020-12-31 17:38:42 -0500

    updated weather api

--- open_weather_api.py
+++ open_weather_api.py
@@ -4,28 +4,36 @@ default location = toronto
 """
 
 import requests
-from credentials import weather_api_key
+from credentials import WEATHER_API_KEY
 
 TORONTO_LAT = '43.6533'
 TORONTO_LONG = '-79.3841'
 
-def weather(lat=TORONTO_LAT, lon=TORONTO_LONG, part='', api_key=weather_api_key):
+def current_weather(lat=TORONTO_LAT, lon=TORONTO_LONG, part='', api_key=WEATHER_API_KEY):
     
-    # OpenWeather One Call API
-    api_call = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
-    response = requests.get(api_call)
+    try:
+        # OpenWeather One Call API
+        api_call = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
+        response = requests.get(api_call)
+        response.raise_for_status()
 
-    # convert json to dictionary
-    response_json = response.json()
-    return response_json
+        # convert json to dictionary
+        return response.json()
+    
+    except Exception as ex:
+        raise ex
 
 
-def pollution(lat=TORONTO_LAT, lon=TORONTO_LONG, api_key=weather_api_key):
+def current_pollution(lat=TORONTO_LAT, lon=TORONTO_LONG, api_key=WEATHER_API_KEY):
 
-    # OpenWeather Pollution API
-    api_call = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
-    response = requests.get(api_call)
+    try:
+        # OpenWeather Pollution API
+        api_call = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+        response = requests.get(api_call)
+        response.raise_for_status()
 
-    # convert json to dictionary
-    response_json = response.json()
-    return response_json
+        # convert json to dictionary
+        return response.json()
+    
+    except Exception as ex:
+        raise ex
