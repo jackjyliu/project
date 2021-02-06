f4b17967de1ae2ee1f5c3a1f2a2aba72e3ae60ee
Jack <jack.jy.liu@gmail.com>
2021-02-05 23:01:11 -0500

    add comment section with psql write

--- todata/views/about.py
+++ todata/views/about.py
@@ -1,8 +1,24 @@
-from todata import app, cache
-from flask import render_template
+from todata import app
+from flask import render_template, request
+from todata.models.sql.functions import sql_write
 
 
-@app.route("/about")
-@cache.cached(timeout=60)
+@app.route("/about", methods=['POST', 'GET'])
 def about():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        comment = request.form.get("comment")
+        
+        # write to email and comment and timestamp to db
+        write_db = "toronto"
+        query = """
+                BEGIN;
+                INSERT INTO website_comments (ts, email, user_comment) VALUES (CURRENT_TIMESTAMP, %s, %s);
+                COMMIT;
+                """
+        records = (email, comment)
+        sql_write(write_db, query, records, single_insert=True)
+
+        return render_template("thanks.html")
+
     return render_template("about.html")
