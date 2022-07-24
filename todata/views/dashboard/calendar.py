ad152c58704a8c12df9c32ff0c49ec52420fddb3
Jack <jack.jy.liu@gmail.com>
2022-07-24 15:33:24 -0400

    updated calendar

--- todata/views/dashboard/calendar.py
+++ todata/views/dashboard/calendar.py
@@ -1,18 +1,18 @@
 CALENDAR = [
-    {'date': 'May 23'
-    ,'description': 'Victoria Day'},
-    {'date': 'June 2'
-    ,'description': 'Ontario Election deadline'},
-    {'date': 'June 11'
-    ,'description': 'End of remaining public health measures'},
-    {'date': 'Jul 1'
-    ,'description': 'Canada Day'},
     {'date': 'Aug 1'
     ,'description': 'Civic Day'},
     {'date': 'Sep 6'
     ,'description': 'Labour Day'},
+    {'date': 'Sep 30'
+    ,'description': 'Truth and Reconciliation'},    
     {'date': 'Oct 11'
     ,'description': 'Thanksgiving'},
     {'date': 'Oct 24'
     ,'description': 'Toronto Mayoral Election'},
+    {'date': 'Nov 11'
+    ,'description': 'Remembrance Day'},
+    {'date': 'Dec 25'
+    ,'description': 'Christmas'},
+    {'date': 'Dec 26'
+    ,'description': 'Boxing Day'}
 ]
